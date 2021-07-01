2fecefbaaab7f113effa3e641f405995b4064db2
madelinekreiff <madelinekreiff@gmail.com>
2021-07-01 15:33:37 -0700

    Added a keyup event listener and added/removed the .playing class

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
+// select audio tags & .key div elements (this will create arrays)
+const audio = document.querySelectorAll("audio");
+const keyDivs = document.querySelectorAll(".key");
+
 // create array for key codes of the letters
-const keyCodes = document.querySelectorAll("div [data-key]")
 const keyCodesArray = [];
-for (let i of keyCodes) {
+for (let i of keyDivs) {
     keyCodesArray.push(i.dataset.key);
 }
 
-// array for audio tags
-const audio = document.querySelectorAll("audio");
-
-// keydown event listener - use key codes to see if letters match then play corresponding audio
-document.addEventListener("keydown", function(e) {
+// keydown event listener - use key codes to see if letters match then play corresponding audio and add class .playing
+document.addEventListener("keydown", function (e) {
     const keyCodeVal = e.keyCode;
     keyCodesArray.forEach(function (keyFromArray, index) {
         if (keyFromArray == keyCodeVal) {
+            keyDivs[index].classList.add("playing");
             audio[index].play();
+        }
+    });
+});
+
+// keyup event listener to remove .playing class
+document.addEventListener("keyup", function (e) {
+    const keyCodeVal = e.keyCode;
+    keyCodesArray.forEach(function (keyFromArray, index) {
+        if (keyFromArray == keyCodeVal) {
+            keyDivs[index].classList.remove("playing");
         }
     });
 });
